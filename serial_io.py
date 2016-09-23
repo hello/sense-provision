ddb49b752e79455719fad3924d636e8a7c2311b7
JChen <jackson.jl.chen@gmail.com>
2016-09-22 18:23:23 -0700

    OOP~

--- serial_io.py
+++ serial_io.py
@@ -3,51 +3,77 @@ from serial.tools import list_ports
 from logger import loge, logi
 
 
-sig_abort = False
-def serial_abort():
-    sig_abort = True
-    
-def get_port():
-    ports = list_ports.comports()
-    if len(ports) > 0:
+class SerialIO:
+    def __init__(self):
+        self.abort = False
         try:
-            dev = ports[0].device
-            logi("Opening port %s"%(dev))
-            port = serial.Serial(port = dev,
-                                 baudrate = 115200,
-                                 parity = serial.PARITY_NONE,
-                                 bytesize=serial.EIGHTBITS,
-                                 stopbits=serial.STOPBITS_ONE,
-                                 timeout = 1)
-            return port
-        except Exception as e:
-            loge("Unable to Open Com Port, Error %s"%(e))
-
-def read_line(p):
-    global sig_abort
-    line = []
-    while True:
-        c = p.read()
-        if len(c) is 0:
-            if sig_abort:
-                return
-            else:
-                continue
-        if ord(c) is not ord('\n'):
-            line.append(c)
-        else:
-            return "".join(line).rstrip()
+            self.__get_port()
+        except:
+            raise
+        
+    def __del__(self):
+        self.close()
             
+    def close(self):
+        self.port.close()
+            
+    def abort(self):
+        self.abort = True
+
+    def __get_port(self):
+        ports = list_ports.comports()
+        if len(ports) > 0:
+            try:
+                dev = ports[0].device
+                logi("Opening port %s"%(dev))
+                self.port = serial.Serial(port = dev,
+                                     baudrate = 115200,
+                                     parity = serial.PARITY_NONE,
+                                     bytesize=serial.EIGHTBITS,
+                                     stopbits=serial.STOPBITS_ONE,
+                                     timeout = 1) 
+            except Exception as e:
+                loge("Unable to Open Com Port, Error %s"%(e))
+                raise e
+
+    def read_line(self):
+        line = []
+        while True:
+            c = self.port.read()
+            if len(c) is 0:
+                if self.abort:
+                    return
+                else:
+                    continue
+            if ord(c) is not ord('\n'):
+                line.append(c)
+            else:
+                return "".join(line).rstrip()
+
+    def write_command(self, cmd):
+        fmt_cmd = cmd.rstrip()+"\r\n"
+        try:
+            if len(fmt_cmd) == p.write(fmt_cmd):
+                #logi("Input command %s Success"%cmd.rstrip())
+                return True           
+        except:
+            pass
+        loge("Input command %s Failure"%cmd.rstrip())
+        return False
+
+    
+
 def test_port():
     import signal
+    sig_abort = False
     def signal_handler(signal, frame):
-        global sig_abort
         sig_abort = True
-    p = get_port()
-    if p:
-        while True and not sig_abort:
-            logi(read_line(p))
-        p.close()
+    p = SerialIO()
+    while True and not sig_abort:
+        line = p.read_line()
+        logi(line)
+        if "SYNC_DEVICE_ID" in line:
+            p.write_command("genkey")
 
 test_port()
         
