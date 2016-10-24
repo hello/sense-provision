97fe30eb3efa2ca8fa31089c3e5ab24c679155cd
Jackson Chen <jchen@hello.is>
2016-10-24 14:17:54 -0700

    command specific behavior

--- autobot.py
+++ autobot.py
@@ -10,17 +10,48 @@ AutobotCommand("command", "expected", tieout),
 ]
 
 """
-class AutobotCommand:
+class AutobotCommand(object):
+    def __init__(self, name = ""):
+        self.completed = False #True if command has finished
+        self.status = "FAIL"
+        self.name = name
+
+    def finish(self, status = "PASS"):
+        self.completed = True
+        self.status = status
+
+    def execute(self, io):
+        raise Exception("execute function not implemented")
+
+    def get_status_string(self):
+        if self.completed:
+            return "%s - %s"%(self.name, self.status)
+        else:
+            return "%s - FAIL(Incomplete)"%(self.name)
+
+    def did_pass(self):
+        return self.status == "PASS"
+
+class TextCommand(AutobotCommand):
     def __init__(self, command, expected, timeout):
-        self.completed = False
+        super(TextCommand, self).__init__(name=command)
         self.command = command
         self.expected = expected
         self.timeout = timeout
-        self.status = "Incomplete"
+        
+    def execute(self, io):
+        try:
+            io.write_command(self.command)
+            while True:
+                line = io.read_line(self.timeout)
+                if line == self.expected:
+                    self.finish()
+                    return True
+        except Exception as e:
+            loge("Command Error %s"%(e))
+            pass
+        return False
 
-    def complete(self, status = "Completed"):
-        self.completed = True
-        self.status = status
         
 class Autobot:
     def __init__(self, io, commands, verbose = False):
@@ -32,44 +63,30 @@ class Autobot:
         
     def run(self):
         for command in commands:
-            if not self.execute(command):
+            if not command.execute(self.io):
                 break
         self.print_status()
 
-    def execute(self, command):
-        try:
-            self.io.write_command(command.command)
-            while True:
-                line = self.io.read_line(command.timeout)
-                if self.verbose:
-                    logi(line)
-                if line == command.expected:
-                    command.complete()
-                    return True
-        except Exception as e:
-            loge("Command Error %s"%(e))
-            pass 
-        return False
 
     def print_status(self):
         logi("Result:")
         allpass = True
         for command in commands:
-            logi("Command %s Status %s"%(command.command, command.status))
-            if not command.completed:
+            logi("%s"%(command.get_status_string()))
+            if not command.did_pass():
                 allpass = False
         if allpass:
-            logi("PASS")
+            logi("!!!PASS!!!")
         else:
-            logi("FAIL")
+            logi("!!!FAIL!!!")
 
 
 
 if __name__ == "__main__":
     commands = [
-        AutobotCommand("connect Hello godsavethequeen 2", "SL_NETAPP_IPV4_ACQUIRED", 10),
-        AutobotCommand("x $is3-us-west-1.amazonaws.com/hello-firmware-public/Rev5/SLPTONES/ST007.raw $f/SLPTONES/ST007.raw", "Cmd Stream transfer exited with code -2", 30),
-        AutobotCommand("disconnect", "SL_WLAN_DISCONNECT_EVENT", 10)
+        TextCommand("connect Hello godsavethequeen 2", "SL_NETAPP_IPV4_ACQUIRED", 10),
+        TextCommand("x $is3-us-west-1.amazonaws.com/hello-firmware-public/Rev5/SLPTONES/ST007.raw $f/SLPTONES/ST007.raw", "Cmd Stream transfer exited with code -2", 30),
+        TextCommand("disconnect", "SL_WLAN_DISCONNECT_EVENT", 10)
         ]
     bot = Autobot(SenseIO(), commands, verbose = True)
     bot.run()
