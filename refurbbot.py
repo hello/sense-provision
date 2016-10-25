cc8f4c2cba5f6565a89b753291f33f490921cc51
Jackson Chen <jchen@hello.is>
2016-10-25 11:04:53 -0700

    dfu

--- refurbbot.py
+++ refurbbot.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         DelayCommand(2),
         GenKeyCommand(),
         ProvisionCommand(),
+        TextCommand("^ dfu", "got SYNC_DEVICE_ID", 60, fuzzy = True)
         ]
     bot = Autobot(SenseIO(), commands, verbose = True)
     if bot.run():
