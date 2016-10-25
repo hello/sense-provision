f630310f2afadcbddc1c6fb26ee3b86bc8e6cec5
Jackson Chen <jchen@hello.is>
2016-10-25 14:54:50 -0700

    update

--- refurbbot.py
+++ refurbbot.py
@@ -2,14 +2,22 @@
 from autobot import *
 import sys
 if __name__ == "__main__":
-    commands = [
-        TextCommand("^ bounce", "", 10),
-        IDSNCommand(color = "B"),
-        DelayCommand(2),
-        GenKeyCommand(),
-        ProvisionCommand(),
-        TextCommand("^ dfu", "got SYNC_DEVICE_ID", 60, fuzzy = True)
-        ]
+    if len(sys.argv) > 1 and sys.argv[1] == "new":
+        commands = [
+            TextCommand("^ bounce", "", 10),
+            IDSNCommand(color = "B"),
+            DelayCommand(2),
+            GenKeyCommand(),
+            ProvisionCommand(),
+            TextCommand("^ dfu", "got SYNC_DEVICE_ID", 60, fuzzy = True)
+            TextCommand("^ bounce", "", 10),
+            MinitermCommand()
+            ]
+    else:
+        commands = [
+            MinitermCommand()
+            ]
+            
     bot = Autobot(SenseIO(), commands, verbose = True)
     if bot.run():
         sys.exit(0)
