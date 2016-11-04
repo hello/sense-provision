103eefc784a3ef6f2035b66e3a32211fef5eccf0
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2016-11-04 16:22:11 -0700

    test

--- autobot/speechtest.py
+++ autobot/speechtest.py
@@ -5,21 +5,34 @@ import os
 
 class FolderWalker():
     def __init__(self, root):
-        self.root = root
-        self.root_files = os.listdir(self.root)
+        self.root_files =  iter([os.path.join(root, f) for f in os.listdir(root) if "wav" in f.lower()])
 
     def __str__(self):
-        f = os.path.join(self.root, "oksense_1.wav")
-        return f
+        name = self.root_files.next()
+        return name
+
+class OKCounter:
+    def __init__(self):
+        self.passcount = 0
+
+    def on_match(self, match): #match is the regex match object
+        self.passcount += 1
+
+okcounter = OKCounter()
+testcount = 3
 
 commands = [
         Text("boot"),
-        Repeat( 2,
-            Conditional(Conditional.ALL,
+        Repeat( testcount,
+            Conditional(Conditional.ANY,
                 Sound(FolderWalker( os.path.join(PROJECT_ROOT, "assets", "audio", "oksense"))),
-                Search("OKAY_SENSE", timeout = 1),
+                Search("OKAY SENSE", handler = okcounter, timeout = 1),
                 ),
             )
         ]
 
 Autobot(SenseIO(), commands).run()
+logi("Passed %d Out of %d tests, %f"%(okcounter.passcount, testcount, okcounter.passcount * 1.0/testcount * 100))
+
+
+
